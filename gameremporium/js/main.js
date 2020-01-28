da6419feeef28582db36a0de93794eb8727281da
LonsterMonster <lonniegibson16@gmail.com>
2020-01-28 00:20:32 -0500

    Update main.js

--- gameremporium/js/main.js
+++ gameremporium/js/main.js
@@ -4,9 +4,11 @@
 	// Mobile Nav toggle
 	$('.menu-toggle > a').on('click', function (e) {
 		e.preventDefault();
-		$('#responsive-nav').addClass('active');
+		
 		if($('#responsive-nav').hasClass("active"){
 			$('#responsive-nav').removeClass('active');
+		}else{
+		$('#responsive-nav').addClass('active');
 		}
 	})
 
